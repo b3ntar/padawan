57c8226795752cd2dc13a2c5bbe1730fed41c2f8
b3ntar <78212165+b3ntar@users.noreply.github.com>
2021-01-29 20:06:09 +0200

    Update arr doubled.js

--- arr doubled.js
+++ arr doubled.js
@@ -1,22 +1,16 @@
-const doubled = (arr) => {
-  
+const doubled = (arr) => { 
     let array = []
-    
-    for (i = 0; i < arr.length; i++) {
-    
+      for (i = 0; i < arr.length; i++) {
     arr1.push(arr[i] * 2)
     }
     
     return array
   }
-  
   console.log(doubled([1, 2, 3, 4])) // => [2, 4, 6, 8]
-  console.log(doubled([5, 5])) // => [10, 10]
-  
+  console.log(doubled([5, 5])) // => [10, 10
   test('test doubled', () => {
     expect(doubled([1, 2, 3, 4])).toBe([2, 4, 6, 8]);
   });
-  
   test('test doubled#2', () => {
     expect(doubled([5, 5])).toBe([10, 10]);
   });
